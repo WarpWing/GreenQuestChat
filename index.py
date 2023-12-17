55b06b56765e17ead91107e6d1d43404a8a61a4f
Ty Chermsirivatana <professornitro@gmail.com>
2023-12-17 18:30:55 -0500

    Update index.pyto v1.0.5 - Using Optimum Embeddings

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import streamlit as st
 import openai
 import logging
 import sys
-import phoenix as px
 import llama_index
 from qdrant_client import QdrantClient
 from llama_index import VectorStoreIndex, ServiceContext
@@ -10,10 +9,10 @@ from llama_index.llms import OpenAI
 from llama_index import SimpleDirectoryReader
 from llama_index.storage.storage_context import StorageContext
 from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.callbacks import CallbackManager, OpenInferenceCallbackHandler
+from llama_index.embeddings import OptimumEmbedding
+from llama_index import set_global_service_context
 
-
-version = "1.0.4"
+version = "1.0.5"
 st.set_page_config(page_title=f"Gaia v{version}", page_icon="🌎", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets["openai_key"]
 st.title(f"Gaia v{version}")
@@ -29,10 +28,18 @@ session = px.launch_app()
 #Setup Tracing for Global Handlers
 llama_index.set_global_handler("arize_phoenix")
 
+# Create Embedding Model
+OptimumEmbedding.create_and_save_optimum_model(
+    "BAAI/bge-small-en-v1.5", "./bge_onnx"
+)
 
+embed_model = OptimumEmbedding(folder_name="./bge_onnx")
+
+
+# Update Custom QA Template with Gaia Information
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Hello, My name is Gaia! I am here to answer any questions you have about Dickinson College Sustainability Efforts and/or GreenQuest Solutions."}
+        {"role": "assistant", "content": "Hello, I'm Gaia, ready to assist with any inquiries about Sustainability at Dickinson College. Feel free to ask me about the Center for Sustainability Education (CSE), Alliance for Aquatic Resource Monitoring (ALLARM), Dickinson's Carbon Neutrality objectives, or Strategic Sustainability Plans."
     ]
 
 @st.cache_resource(show_spinner=False)
@@ -43,9 +50,10 @@ def load_data():
             url=st.secrets["qdrant_url"], 
             api_key=st.secrets["qdrant_key"],
         )
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, callback_manager = CallbackManager([callback_handler]), system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response. If they ask how you are made, give them the response as if they asked if how Gaia was made. Gaia can also respond in any language including but not limited to English,Spanish,German,Dutch,Chinese,Thai,Korean and Japanese.  Try to keep translation short to about 4-5 sentences. If you don't know the answer, you can say 'I don't know' or 'I am not sure'."))
+        service_context = ServiceContext.from_defaults((embed_model=embed_model,llm=OpenAI(model="gpt-4", max_tokens=1500, temperature=0.5, callback_manager = CallbackManager([callback_handler]), system_prompt="Keep your answers technical and based on facts and do not hallucinate in responses. In addition, make sure all responses look natural, no Answer: or Query: in the response. If they ask how you are made, give them the response as if they asked if how Gaia was made. Gaia can also respond in any language including but not limited to English,Spanish,German,Dutch,Chinese,Thai,Korean and Japanese.  Try to keep translation short to about 4-5 sentences. If you don't know the answer, you can say 'I don't know' or 'I am not sure'."))
         vector_store = QdrantVectorStore(client=qdrant_client, collection_name="gaiafinal")
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        set_global_service_context(service_context)
         index = VectorStoreIndex.from_documents(
             docs, storage_context=storage_context, service_context=service_context,
         )
